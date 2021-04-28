cb3197f6cc031b6aece6fca836dc59c6ca238219
JesterMoonDesign <82544432+JesterMoonDesign@users.noreply.github.com>
2021-04-28 03:47:25 +0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,33 @@
+"use strict"
+
+const isMobile = {
+	Andorid: function () {
+		return navigator.userAgent.match(/Andorid/i);
+	},
+	BlackBerry: function () {
+		return navigator.userAgent.match(/BlackBerry/i);
+	},
+	iOS: function () {
+		return navigator.userAgent.match(/iPhone|iPad|iPod/i);
+	},
+	Opera: function () {
+		return navigator.userAgent.match(/Opera Mini/i);
+	},
+	Windows: function () {
+		return navigator.userAgent.match(/IEMobile/i);
+	},
+	any: function () {
+		return (
+			isMobile.Andorid() ||
+			isMobile.BlackBerry() ||
+			isMobile.iOS() ||
+			isMobile.Opera() ||
+			isMobile.Windows());
+	}
+};
+
+if (isMobile.any()) {
+	document.body.classList.add('_touch');
+} else {
+	document.body.classList.add('_pc');
+}
