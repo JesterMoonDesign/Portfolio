ec302ad77af06e4b929b5879b4af6519a0aee83c
JesterMoonDesign <82544432+JesterMoonDesign@users.noreply.github.com>
2021-05-18 02:20:14 +0300

    Add files via upload

--- scripts/script.js
+++ scripts/script.js
@@ -18,11 +18,11 @@ const isMobile = {
 	},
 	any: function () {
 		return (
-			isMobile.Andorid() ||
-			isMobile.BlackBerry() ||
-			isMobile.iOS() ||
-			isMobile.Opera() ||
-			isMobile.Windows());
+		isMobile.Andorid() ||
+		isMobile.BlackBerry() ||
+		isMobile.iOS() ||
+		isMobile.Opera() ||
+		isMobile.Windows());
 	}
 };
 
@@ -32,16 +32,12 @@ if (isMobile.any()) {
 	document.body.classList.add('_pc');
 }
 
-const menuBurger = document.querySelector('.burger-list');
-
-const burger = document.querySelector('.burger');
+const menuBurger = document.querySelector('.burger-list'),
+		burger = document.querySelector('.burger');
 
 burger.addEventListener('click', () => {
 	menuBurger.classList.toggle('active');
 });
-
-
-
 
 const prev = document.getElementById('btn-prev'),
 		next = document.getElementById('btn-next'),
@@ -49,8 +45,8 @@ const prev = document.getElementById('btn-prev'),
 		dots = document.querySelectorAll('.frame-count-dot');
 
 let index = 0,
-	slide = document.querySelector('.slide'),
-	dot = document.querySelector('.frame-count-dot');
+	 slide = document.querySelector('.slide'),
+	 dot = document.querySelector('.frame-count-dot');
 
 const prepareCurrentSlide = ind => {
 	activeSlide(ind);
@@ -59,59 +55,57 @@ const prepareCurrentSlide = ind => {
 
 const activeDot = n => {
 	for(dot of dots) {
-		dot.classList.remove('active');
+	dot.classList.remove('active');
 	}
 	dots[n].classList.add('active');
 }
 
 const nextDot = () => {
 	if(index == slides.length - 1) {
-		index = 0;
-		activeSlide(index);
+	index = 0;
+	activeSlide(index);
 	} else {
-		index++;
-		activeSlide(index);
+	index++;
+	activeSlide(index);
 	}
 }
 
 const activeSlide = n => {
 	for(slide of slides) {
-		slide.classList.remove('active');
+	slide.classList.remove('active');
 	}
 	slides[n].classList.add('active');
 }
 
 const nextSlide = () => {
 	if(index == slides.length - 1) {
-		index = 0;
+	index = 0;
 	prepareCurrentSlide(index);
 	} else {
-		index++;
+	index++;
 	prepareCurrentSlide(index);
 	}
 }
 
 const prevSlide = () => {
 	if(index == 0) {
-		index = slides.length - 1;
+	index = slides.length - 1;
 	prepareCurrentSlide(index);
 	} else {
-		index--;
+	index--;
 	prepareCurrentSlide(index);
 	}
 }
 
 dots.forEach ((item, indexDot) => {
 	item.addEventListener('click', () => {
-		index = indexDot;
-		prepareCurrentSlide(index);
+	index = indexDot;
+	prepareCurrentSlide(index);
 	});
 })
 
 next.addEventListener('click', nextSlide);
 prev.addEventListener('click', prevSlide);
-
-
 
 const contactsBtn = document.querySelector('.contacts-btn');
 let contactsPage =  document.querySelector('.contacts-wraper');
@@ -119,3 +113,88 @@ let contactsPage =  document.querySelector('.contacts-wraper');
 contactsBtn.addEventListener('click', () => {
 	contactsPage.classList.toggle('active');
 });
+
+
+
+
+
+const gallery = document.querySelector('.gallery'),
+		galleryResize = document.querySelector('.gallery_resize'),
+		sliderSwitch = document.querySelector('.slider_switch'),
+		sliderSwitchPosition = getComputedStyle(sliderSwitch),
+		sliderSwitchImg = document.querySelector('.slider_switch_img'),
+		sliderSwitchImgPosition = getComputedStyle(sliderSwitchImg);
+
+function galleryResizeWidth() {
+	(galleryResize.style.width = parseInt(sliderSwitchImgPosition.marginLeft) + 20 + 'px')
+}
+
+galleryResizeWidth()
+
+// Для пк:
+function sliderBeeforeAfterPC () {
+
+	sliderSwitchImg.onmousedown = function (event) {
+
+		function onDrag(event) {
+		sliderSwitchImg.style.marginLeft = event.layerX + 'px';
+		galleryResizeWidth();
+		};
+
+		sliderSwitch.addEventListener('mousemove', onDrag);
+
+		sliderSwitch.onmouseleave = function () {
+		sliderSwitch.removeEventListener('mousemove', onDrag);
+		sliderSwitchImg.style.marginLeft = parseInt(sliderSwitchPosition.width) / 2 + 'px';
+		sliderSwitchImg.classList.add('tr');
+		galleryResize.classList.add('tr');
+		galleryResizeWidth();
+		}
+
+		sliderSwitch.onmouseup = function () {
+		sliderSwitch.removeEventListener('mousemove', onDrag);
+		sliderSwitchImg.style.marginLeft = parseInt(sliderSwitchPosition.width) / 2 + 'px';
+		galleryResizeWidth();
+		}
+		galleryResizeWidth();
+	};
+
+		sliderSwitchImg.ondragstart = function() {
+		return false;
+		};
+};
+
+
+//Для смартфона:
+function sliderBeeforeAfterMobile () {
+
+	sliderSwitchImg.addEventListener('touchstart', sliderWork, {passive: true});
+
+		function sliderWork (event) {
+
+		function onDrag(event) {
+		sliderSwitchImg.style.marginLeft = event.touches[0].pageX - event.touches[0].target.offsetLeft + 'px';
+		galleryResizeWidth();
+		};
+
+		sliderSwitch.addEventListener('touchmove', onDrag, {passive: true});
+
+		sliderSwitch.addEventListener('touchend', touchEnd, {passive: true});
+
+		function touchEnd() {
+		sliderSwitch.removeEventListener('touchmove', onDrag, {passive: true});
+		sliderSwitchImg.style.marginLeft = parseInt(sliderSwitchPosition.width) / 2 + 'px';
+		galleryResizeWidth();
+		}
+
+		sliderSwitchImg.ondragstart = function() {
+		return false;
+		};
+	};
+};
+
+if (isMobile.any()) {
+	sliderBeeforeAfterMobile ();
+} else {
+	sliderBeeforeAfterPC ();
+}
